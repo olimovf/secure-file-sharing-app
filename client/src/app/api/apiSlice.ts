d0475af38bca021bc8d4e3116868c20cb929d52c
Fayozbek Olimov <fayozbekdeveloper@gmail.com>
2024-05-13 09:57:07 +0500

    production bugs fixed

--- client/src/app/api/apiSlice.ts
+++ client/src/app/api/apiSlice.ts
@@ -7,7 +7,7 @@ import {
 } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../../features/auth/authSlice';
 
-const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'https://secure-file-sharing-app.onrender.com';
 
 const baseQuery = fetchBaseQuery({
 	baseUrl: BASE_URL,
